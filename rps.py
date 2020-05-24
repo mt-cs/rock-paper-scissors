186904d861261fadb92c440a2f31afdc3d3b0b77
Marisa Tania <60201466+mt-cs@users.noreply.github.com>
2020-05-23 19:42:17 -0700

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,97 +1,105 @@
 import random
+
+# Create global constant
+COMPUTER_WINS = 1
+PLAYER_WINS = 2
+TIE = 0
+INVALID = 3
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
  
+# Value returning function that takes in the argument choice 
+# (the number representing rock, paper, or scissors) 
+# and return as strings
+def choiceString(choice):
+	if choice == ROCK:
+		return 'Rock'
+	if choice == PAPER:
+		return 'Paper'
+	if choice == SCISSORS:
+		return 'Scissors'
+	else:
+		return 'Something went wrong'
+
+# This function takes in integers of choice as arguments,
+# returns one of the following values: 
+# TIE, PLAYER_WINS, COMPUTER_WINS, INVALID	
+def rockPaperScissors(computer, player):
+	if (player == ROCK and computer == SCISSORS):
+		return PLAYER_WINS
+	elif (player == SCISSORS and computer == PAPER):
+		return PLAYER_WINS
+	elif (player == PAPER and computer == ROCK):
+		return PLAYER_WINS
+	elif (computer == ROCK and player == SCISSORS):
+		return COMPUTER_WINS
+	elif (computer == SCISSORS and player == PAPER):
+		return COMPUTER_WINS
+	elif (computer == PAPER and player == ROCK):
+		return COMPUTER_WINS
+	elif (computer == ROCK and player == ROCK):
+		return TIE
+	elif (computer == PAPER and player == PAPER):
+		return TIE
+	elif (computer == SCISSORS and player == SCISSORS):
+		return TIE
+	else:
+		return INVALID   
+
 def main():
-    player_wins = 0
-    computer_wins = 0
+ # 0 is a placeholder and will not be used.
+	player_wins = 0
+	computer_wins = 0
  
-    # This loop will run the game 5 times that count
-    # and keep track of how many wins.
-    # There must be a winner who wins five times
-    while player_wins != 5 and computer_wins != 5:
+ # This loop will run the game 5 times that count
+ # and keep track of how many wins.
+ # There must be a winner who wins five times
+	while player_wins != 5 and computer_wins != 5:
  
-        # Computer chooses a random number.
-        comp_number = random.randint(1, 3)
- 
-        # Initialize value of comp_choice variable
-        # corresponding to the comp_number value.
-        if comp_number == 1:
-            comp_choice = 'Rock'
-        elif comp_number == 2:
-            comp_choice = 'Paper'
-        else:
-            comp_choice = 'Scissor'
- 
-        # User enter their numeric choice from a menu.
-        user_number = int(input('Enter 1 for rock, 2 paper, 3 scissors: '))
- 
-        # Check user's input using a validation loop.
-        while user_number > 3 or user_number < 1:
-            print('Please choose a value of 1, 2, or 3')
-            print()
-            user_number = int(input('Enter 1 for rock, 2 paper, 3 scissors: '))
- 
-        # Get the value in string.
-        choice_string = choiceString(user_number)
- 
-        # Get the computer's and user's choices.
-        rock_paper_scissors = rockPaperScissors(comp_choice, choice_string)
- 
-        # Display user's choice
-        print('You chose ' + choice_string)
- 
-        # Display computer's choice
-        print('Computer chose ' + comp_choice)
- 
-        # Determine and display who wins this game
-        # Calculate computer's and user's score
-        if rock_paper_scissors == 1:
-            print('The computer wins the game')
-            print()
-            computer_wins = computer_wins + 1
-        elif rock_paper_scissors == 2:
-            print('You win the game')
-            print()
-            player_wins = player_wins + 1
-        elif rock_paper_scissors == 0:
-            print ('You made the same choice as computer. Starting over')
-            print ()
+		# Computer chooses a random number.
+		computer = random.randint(1, 3)
 
-    # Display score
-    print('After 5 runs, the totals are:')
-    print('You\t', player_wins)
-    print('Computer', computer_wins)
+		# User enter their numeric choice from a menu.
+		player = int(input('Enter 1 for rock, 2 paper, 3 scissors: '))
+  
+  # Check user's input using a validation loop.
+		while player > 3 or player < 1:
+			print('Please choose a value of 1, 2, or 3')
+			print()
+			player = int(input('Enter 1 for rock, 2 paper, 3 scissors: '))
+ 
+		# Call choiceString(choice) function to print the player’s hand
+		playerString = choiceString(player)
+		print('Player chose:',playerString)
+
+		# Call choiceString(choice) function to print the computer’s hand
+		computerString = choiceString(computer)
+		print('Computer chose:',computerString)
+  
+  # Call your value returning function rockPaperScissors(computer, player)
+		# to get the result of the round between the computer and player.
+		result = rockPaperScissors(computer, player)
+  
+  # Show player if the result is tie
+		if result == TIE:
+			print('You made the same choice as the computer. Starting over\n')
+		# Declare if the computer is the winner
+		elif result == COMPUTER_WINS:
+			print('The computer wins the game\n')
+			computer_wins = computer_wins + 1
+		# Declare if the player is the winner
+		elif result == PLAYER_WINS:
+			print('The player wins the game\n')
+			player_wins = player_wins + 1
+		# Declare invalid input
+		elif result == INVALID:
+			print('You made an invalid choice. No winner\n')
+ 
+ # Display score
+	print('After 5 runs, the totals are:')
+	print('You\t', player_wins)
+	print('Computer', computer_wins)
+
+# Call the main function
 main ()
-
-# The choiceString function receives the choice number
-# and returns that value in string
-def choiceString (choice):
-    if choice == 1:
-        return 'Rock'
-    elif choice == 2:
-        return 'Paper'
-    else:
-        return 'Scissor'
-   
-# The rockPaperScissors function receives numbers
-# representing the computer's and player's choices.
-def rockPaperScissors (computer, player):
-    if (player == 'Rock' and computer == 'Scissor'):
-        return 2
-    elif (player == 'Scissor' and computer == 'Paper'):
-        return 2
-    elif (player == 'Paper' and computer == 'Rock'):
-        return 2
-    elif (computer == 'Rock' and player == 'Scissor'):
-        return 1
-    elif (computer == 'Scissor' and player == 'Paper'):
-        return 1
-    elif (computer == 'Paper' and player == 'Rock'):
-        return 1
-    elif (computer == 'Rock' and player == 'Rock'):
-        return 0
-    elif (computer == 'Paper' and player == 'Paper'):
-        return 0
-    elif (computer == 'Scissor' and player == 'Scissor'):
-        return 0      
-           
-      
